1e4c430b6c95f875069ba28c13709204261fc76e
Tim Anderson <t.anderson0085@gmail.com>
2022-08-28 12:43:03 -0500

    movie item and movie list linked getting error in fetchMovie

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -2,11 +2,31 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import React from 'react';
 
-function MovieItem () {
+function MovieItem ({movie}) {
 
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    //select a movie
+    const handleSelect = () => {
+        console.log('handleSelect');
+
+        //get selected movie info
+        dispatch({ type: 'FETCH_DETAILS', payload: movie.id });
+        dispatch({ type: 'FETCH_GENRE', payload: movie.id });
+
+        //nav to movie details on click
+        history.push(`/details/${movie.id}`);
+    }
 
     return (
-        console.log('MovieItem Page')
+        // console.log('MovieItem Page')
+        <div key = {movie.id} className='title'> 
+            <p>{movie.title}
+            <br></br>
+            <img src={movie.poster} alt={movie.title} onClick={handleSelect} className="image1"/>
+            </p>
+        </div>
     )
 }
 
