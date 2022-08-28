c2079698f47c3742f914f4f98b03cc4a7527e660
Tim Anderson <t.anderson0085@gmail.com>
2022-08-28 09:33:34 -0500

    basic Details page setup

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+
+function Details () {
+
+
+    return (
+        console.log('Details Page')
+    )
+}
+
+
+export default Details;
